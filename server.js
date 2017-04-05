c418471148edfd77baab7d85fcf5cae5ef51297a
raunaqarora <raunaqarora123@gmail.com>
2017-04-05 01:00:29 -0700

    Fixed 404 error

--- server.js
+++ server.js
@@ -15,4 +15,7 @@ var routes = require('./api/routes/routes');
 routes(app);
 
 app.listen(port);
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 console.log('RESTful API server started on: ' + port);
