06acf77770c04ea4b05ca24731f9d61a323edb6b
raunaqarora <raunaqarora123@gmail.com>
2017-04-10 21:05:45 -0700

    Added basic schema

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 500,
-mongoose = require('mongoose'),
+//mongoose = require('mongoose'),
 Task = require('./api/models/model'),
 bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+//mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://localhost/Tododb');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var routes = require('./api/routes/routes');
